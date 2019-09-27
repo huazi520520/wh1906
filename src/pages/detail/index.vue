a9cb492aa9adefe3aa509571fb2caa11e8af525a
huazi <huazi@qq.com>
2019-09-27 09:31:16 +0800

    项目初始化

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>这是一个搜素页面 </h2>
+        <h2>
+            {{msg}}
+        </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "1906 daydayup"
+        };
+    }
+};
+</script>
+
+<style  scoped>
+</style>
